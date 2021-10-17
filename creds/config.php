e7b06c139f779471ae3170e3e126c5733087b118
anthony0214 <38491872+anthony0214@users.noreply.github.com>
2021-10-18 07:56:15 +0800

    Update config.php

--- creds/config.php
+++ creds/config.php
@@ -7,6 +7,6 @@ define('DB_USER', 'root'); //db user
 define('DB_PASS', ''); //db pass
 
 //fb creds
-define('FB_APP_ID', '307589261173634');
-define('FB_APP_SECRET', '03f3e8eda18adf65a89012327c8ffdd7');
-define('FB_REDIRECT_URI', 'http://localhost/login-fb/auth-signin.php');
+define('FB_APP_ID', '<FB ID>');
+define('FB_APP_SECRET', '<FB SECRET>');
+define('FB_REDIRECT_URI', '<RETURN PAGE>');
